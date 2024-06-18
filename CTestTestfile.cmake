d71176eaddb4950035c8bd78a95cf431edd583e4
Harsh <4385695+neoxharsh@users.noreply.github.com>
2024-06-18 15:26:11 +0100

    v 1.0 - First working commit

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/neoha/Documents/PasswordConcealer/dep/clip
+# Build directory: /home/neoha/Documents/PasswordConcealer/dep/clip
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("examples")
+subdirs("tests")
---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/neoha/Documents/PasswordConcealer/dep/clip/tests
+# Build directory: /home/neoha/Documents/PasswordConcealer/dep/clip/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(clip_text_tests "/home/neoha/Documents/PasswordConcealer/dep/clip/tests/clip_text_tests")
+set_tests_properties(clip_text_tests PROPERTIES  RUN_SERIAL "TRUE" _BACKTRACE_TRIPLES "/home/neoha/Documents/PasswordConcealer/dep/clip/tests/CMakeLists.txt;6;add_test;/home/neoha/Documents/PasswordConcealer/dep/clip/tests/CMakeLists.txt;12;add_clip_test;/home/neoha/Documents/PasswordConcealer/dep/clip/tests/CMakeLists.txt;0;")
+add_test(clip_user_format_tests "/home/neoha/Documents/PasswordConcealer/dep/clip/tests/clip_user_format_tests")
+set_tests_properties(clip_user_format_tests PROPERTIES  RUN_SERIAL "TRUE" _BACKTRACE_TRIPLES "/home/neoha/Documents/PasswordConcealer/dep/clip/tests/CMakeLists.txt;6;add_test;/home/neoha/Documents/PasswordConcealer/dep/clip/tests/CMakeLists.txt;13;add_clip_test;/home/neoha/Documents/PasswordConcealer/dep/clip/tests/CMakeLists.txt;0;")
+add_test(clip_image_tests "/home/neoha/Documents/PasswordConcealer/dep/clip/tests/clip_image_tests")
+set_tests_properties(clip_image_tests PROPERTIES  RUN_SERIAL "TRUE" _BACKTRACE_TRIPLES "/home/neoha/Documents/PasswordConcealer/dep/clip/tests/CMakeLists.txt;6;add_test;/home/neoha/Documents/PasswordConcealer/dep/clip/tests/CMakeLists.txt;15;add_clip_test;/home/neoha/Documents/PasswordConcealer/dep/clip/tests/CMakeLists.txt;0;")
